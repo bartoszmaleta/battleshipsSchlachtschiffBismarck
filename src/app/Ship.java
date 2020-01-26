0a662cae339f1c60cf147cd7f5b3f0c5cfd12c9e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-26 20:17:24 +0100

    Add variables

--- src/app/Ship.java
+++ src/app/Ship.java
@@ -1,5 +1,11 @@
 package app;
 
 public class Ship {
-    
+    // private?
+
+    private Coordinate coordinate;
+    private int length;
+    private boolean horizontal;
+    private boolean isHit = false;
+    private int health;
 }
